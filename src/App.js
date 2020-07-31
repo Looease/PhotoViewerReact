0715f45579a832e5bb6c45746a609201358f8663
Louise <louise.gilligan@zoopla.co.uk>
2020-07-31 12:05:42 +0100

    Working React Photo Changer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
+
 import {PhotoViewer} from "./PhotoViewer/PhotoViewer";
 import {Thumbnails} from "./Thumbnails/Thumbnails";
 import * as styles from "./App.module.scss";
 
+
 function App() {
+
+  const [selectedImageURL, updateImageURL] =useState("https://picsum.photos/id/6${imageNumberString}/1600/900")
+
+
   return (
     <main>
       <h1 className={styles.title}>Photo Viewer</h1>
-      <PhotoViewer imageUrl="https://i.picsum.photos/id/223/200/300.jpg?hmac=IZftr2PJy4auHpfBpLuMtFhsxgQYlUgXdV5rFwjGItQ"/>
-      <Thumbnails/>
+      <PhotoViewer imageUrl={selectedImageURL}/>
+      <Thumbnails selectedImageURL={selectedImageURL} updateImageURL={updateImageURL}/>
     </main>
   );
 }
 
 export default App;
+
+
+//anything shared between photoviewer and thumbnails live in here as this is the only class that is connected to both of them
--- src/Thumbnails/Thumbnails.js
+++ src/Thumbnails/Thumbnails.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import * as styles from "./Thumbnails.module.scss";
 import {ImageUrls} from "./imageGenerator";
 
-export const Thumbnails = () => {
+export const Thumbnails = (props) => {
+
     const images = ImageUrls.map(url => 
         <li className={styles.thumbnail}>
-            <img className={styles.thumbnailImage} src={url} alt="Thumbnail"/>
+            <img onClick={() => props.updateImageURL(url)} className={styles.thumbnailImage} src={url} alt="Thumbnail"/>
         </li>
     );
+
     
     return (
         <section className={styles.section}>
             <h2 className={styles.sectionTitle}>Images</h2>
+            <h2>This image is selected {props.selectedImageURL}</h2>
             <ul className={styles.thumbnails}>
                 {images}
             </ul>
